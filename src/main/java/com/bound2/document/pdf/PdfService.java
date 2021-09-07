2b3a065df785d84ce01488efc53036f13902dac6
bound2 <9380102+bound2@users.noreply.github.com>
2021-09-07 22:04:08 +0300

    add pdfservice implementation

--- src/main/java/com/bound2/document/pdf/PdfService.java
+++ src/main/java/com/bound2/document/pdf/PdfService.java
@@ -1,11 +1,33 @@
 package com.bound2.document.pdf;
 
+import com.bound2.provider.WebDriverProvider;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.print.PrintOptions;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 @Service
 public class PdfService {
 
+    private final WebDriverProvider<ChromeDriver> webDriverProvider;
+
+    public PdfService(WebDriverProvider<ChromeDriver> webDriverProvider) {
+        this.webDriverProvider = webDriverProvider;
+    }
+
     public byte[] generate(String base64Html) {
-        return new byte[0];
+        var driver = webDriverProvider.acquire();
+        try {
+            driver.get("data:text/html;base64,%s".formatted(base64Html));
+
+            var printOptions = new PrintOptions();
+            printOptions.setOrientation(PrintOptions.Orientation.PORTRAIT);
+            var result = driver.print(printOptions);
+
+            return Base64.getDecoder().decode(result.getContent());
+        } finally {
+            webDriverProvider.release(driver);
+        }
     }
 }
