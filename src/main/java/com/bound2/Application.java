d20a6168af4901e57baa906cb84df0252d6c7e2d
bound2 <9380102+bound2@users.noreply.github.com>
2021-09-07 20:59:45 +0300

    add spring dependency

--- src/main/java/com/bound2/Application.java
+++ src/main/java/com/bound2/Application.java
@@ -1,4 +1,12 @@
 package com.bound2;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
