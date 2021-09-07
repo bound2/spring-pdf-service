4db384f845967b6d858591cda4bb2de6bdd5bb4c
bound2 <9380102+bound2@users.noreply.github.com>
2021-09-07 22:15:33 +0300

    add api impl and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# spring-pdf-service
+Easily generate PDFs from html using Chrome webdriver & mustache for templating.
+
+## Prerequisites
+* Installed chromedriver on your device (https://chromedriver.chromium.org/downloads)
+  * macOS homebrew `brew install --cask chromedriver` 
+  * Windows chocolatey `choco install chromedriver` 
--- src/main/java/com/bound2/document/DocumentController.java
+++ src/main/java/com/bound2/document/DocumentController.java
@@ -1,9 +1,16 @@
 package com.bound2.document;
 
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class DocumentController {
@@ -20,7 +27,16 @@ public class DocumentController {
     }
 
     @GetMapping("/api/v1/document/{template}/pdf")
-    public void getDocumentPdf(@PathVariable String template) {
-        documentService.createPdf(template);
+    public ResponseEntity<byte[]> getDocumentPdf(@PathVariable String template) {
+        var pdf = documentService.createPdf(template);
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+
+        var disposition = ContentDisposition.attachment()
+                .filename("%s.pdf".formatted(template), StandardCharsets.UTF_8)
+                .build();
+        headers.setContentDisposition(disposition);
+
+        return new ResponseEntity<>(pdf, headers, HttpStatus.OK);
     }
 }
