3f04cbe218834bbfc8421e57be853b58f4d8e051
bound2 <9380102+bound2@users.noreply.github.com>
2021-09-07 22:41:48 +0300

    add webdrver properties

--- src/main/java/com/bound2/Application.java
+++ src/main/java/com/bound2/Application.java
@@ -2,7 +2,11 @@ package com.bound2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan(basePackages = "com.bound2.configuration")
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/com/bound2/configuration/WebDriverProperties.java
@@ -0,0 +1,8 @@
+package com.bound2.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConfigurationProperties(prefix = "bound2.web-driver")
+@ConstructorBinding
+public record WebDriverProperties(Integer concurrentDriverCount){}
